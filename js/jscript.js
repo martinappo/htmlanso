b02b52268723e61a2d93ebe01fd697bbee236a72
martinappo <martinappo@gmail.com>
2014-01-24 13:37:23 +0200

    responsive shit

---
+++ js/jscript.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+/****************************
+*****************************
+
+Main slider
+
+*****************************
+****************************/
+
+$(".slogancontainer").carouFredSel({
+	responsive:true,
+	height: 220,
+	width: "62%",
+	circular: true,
+	infinite: true,
+	scroll: {
+		fx : "fade"
+	},
+	
+	items: {
+		visible: 1,
+		start: "random",
+		height: 220
+	},
+	
+	auto: 3000,
+	next: {
+		button: "#next",
+		key: "right"
+	}
+});
+
+/*** mobile menu **/
+
+$('.openmenu').click(function() {
+	   $(this).toggleClass("open");
+	   $('nav.main ul').toggleClass("open");
+
+});
+
+
+
+
+
+
+
+
+
+
+// nõlvad ************************/
+/** nolvad kirjeldus **/
+
+$('.nolv').click(function() {
+	   $(this).toggleClass("show");
+
+	});
+
+$('.nolv').mouseleave(function() {
+	   $(this).removeClass("show");
+
+	});
+
+
+//fancybox
+
+$("a.fancybox").fancybox();
+
+
+$("#galerii").tabs(	{ 
+fx: { opacity: 'toggle' },
+	select: function(event, ui) {
+		jQuery(this).css('height', jQuery(this).height());
+		jQuery(this).css('overflow', 'hidden');
+	},
+	show: function(event, ui) {
+		jQuery(this).css('height', 'auto');
+		jQuery(this).css('overflow', 'visible');
+	}
+}
+); 
+
+
+//fancybox
+
+$("a.fancybox").fancybox();
+
+
+
+
+}); /* <--- ready function end*/
